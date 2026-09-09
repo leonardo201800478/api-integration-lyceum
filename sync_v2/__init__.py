5dd0e0b82d5931668695efc6f5b1d1adff69db0f
Leonardo Paiva <67243028+leonardo201800478@users.noreply.github.com>
2026-09-09 14:38:15 -0300

    Create Sync V2 package

---
+++ sync_v2/__init__.py
@@ -0,0 +1,7 @@
+"""Sincronização V2 dos endpoints Lyceum.
+
+A V2 é desenvolvida em paralelo à implementação legada em ``sync/``.
+Nenhum sincronizador legado é substituído automaticamente.
+"""
+
+__version__ = "2.0.0-dev"
